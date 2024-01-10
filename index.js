46f4f5a7fd341a041da8dce20f603d13746058ce
NF RIDOY <nfridoy@gmail.com>
2024-01-10 16:24:04 +0600

    Post Api: task

--- index.js
+++ index.js
@@ -28,6 +28,32 @@ async function run() {
         client.connect();
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
+
+        // DB & Collections
+
+        const database = client.db("ToDoDB");
+        const TaskCollection = database.collection("Tasks");
+
+        // APIs
+        app.post("/api/v1/task", async (req, res) => {
+            try {
+                const newTask = req.body;
+
+                const result = await haiku.insertOne(newTask);
+
+                console.log(`A Task was inserted with the _id: ${result.insertedId}`);
+
+                res.send(result);
+            } catch (error) {
+                console.log(error);
+            }
+
+        })
+
+
+
+
+
         client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
